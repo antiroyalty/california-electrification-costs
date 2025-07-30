a60a6cdf2139fc9cf05c8c32d809d00c82d63544
--global <anastasia.santasheva@gmail.com>
2025-07-30 15:49:08 -0700

    comment out simons partial code

--- step6_build_electric_vehicle_load_profiles.py
+++ step6_build_electric_vehicle_load_profiles.py
@@ -11,17 +11,17 @@ import os
 import pandas as pd
 from main_helpers import get_counties, get_scenario_path, log
 
-def download_raw_data():
+# def download_raw_data():
     # TODO: get the data
 
-def cleanup_whatever_we_dont_want():
-    get_names_of_columns
-    make_sure_its_in_the_right_timezone
+# def cleanup_whatever_we_dont_want():
+#     get_names_of_columns
+#     make_sure_its_in_the_right_timezone
 
-reorganize_it_in_other_load_profiles_format
-send_it
-  append_to_file_called electricity_loads_
-  write to column name: "evs"
+# reorganize_it_in_other_load_profiles_format
+# send_it
+#   append_to_file_called electricity_loads_
+#   write to column name: "evs"
 
 
 def process(base_input_dir: str, base_output_dir: str, scenario: str, 
