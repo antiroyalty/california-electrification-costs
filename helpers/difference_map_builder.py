028e3517ab22996da0ffbb913b1b8961f0cce3c6
--global <anastasia.santasheva@gmail.com>
2026-01-12 18:04:52 -0800

    clean up dead code

---
+++ helpers/difference_map_builder.py
@@ -0,0 +1,9 @@
+"""
+Unified Difference Map Builder
+
+This module consolidates difference map generation logic and provides a
+consistent interface for creating difference maps.
+"""
+
+from tools.difference_map_builder import *  # type: ignore
+
